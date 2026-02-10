7287486436721ce7155bb468c27866bcf97c9cb5
mahmoudmoo <mahmoudmoody969@gmail.com>
2026-02-10 21:46:08 +0200

    test commit

--- src/reviews/dtos/reviews.dto.ts
+++ src/reviews/dtos/reviews.dto.ts
@@ -13,6 +13,12 @@ export class ReviewDto {
   rating: number;
 
   @IsNotEmpty()
+  @IsNumber()
+  @Min(1)
+  @Max(5)
+  hamada: number;
+
+  @IsNotEmpty()
   @IsString()
   @Length(5, 500)
   comment: string;
